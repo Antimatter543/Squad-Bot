2b91e6090f4e13e871a7cf885b8275af29d99987
caleb-wishart <caleb.wish@outlook.com>
2020-07-16 18:35:32 +1000

    Commit

---
+++ bot_settings.py
@@ -0,0 +1,13 @@
+# settings.py
+
+elevated_roles_name = ['OG Squad']
+dev_role = ['Developer']
+
+help_text = ("Current commands are:\n```"+
+    "*help [no arg]: Shows this dialogue\n" +
+    "*ping (no arg): Pong!\n" +
+    # "*rollDice (no arg): Rolls a 6 sided die\n" + 
+    # "*flipACoin (no arg): Flips a coin\n" +
+    # "*rollNdice (Integer): Rolls an N sided die\n" +
+    # "(WIP) *reminder (DD:HH:MM:SS): sets a reminder for given time in the future (WIP)\n"+
+    "```")
