86e58f275c04e8ee39f18458894074728122bfc5
caleb-wishart <caleb.wish@outlook.com>
2022-06-05 16:16:54 +1000

    Formatting

--- cogs/general_commands.py
+++ cogs/general_commands.py
@@ -6,15 +6,16 @@ from discord.ext import commands
 
 from random import choice, randint
 
-### TODO update in all files
+# TODO update in all files
 allowed_roles = ['OG Squad']
 admin_roles = ['Developer']
 elevated_roles = ['Mody Boi']
 elevated_roles.extend(admin_roles)
 
+
 class general_commands(commands.Cog):
 
-    def __init__(self,client):
+    def __init__(self, client):
         self.client = client
 
     ### COMMANDS ###
@@ -22,24 +23,24 @@ class general_commands(commands.Cog):
         aliases=['p'],
         brief='Pong!, latency test',
         description='Sends Pong!, followed by the result of a latecny test\nRequires a role of "OG Squad" or higher'
-        )
+    )
     @commands.has_any_role(*allowed_roles)
     async def ping(self, ctx):
         await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
 
     @commands.command(
-        aliases=['6roll','6Roll','6ROLL','Dice','dice'],
+        aliases=['6roll', '6Roll', '6ROLL', 'Dice', 'dice'],
         brief='Roll a dice',
         description='Returns a random number between 1 and 6\nRequires a role of "OG Squad" or higher'
-        )
+    )
     @commands.has_any_role(*allowed_roles)
     async def roll_dice(self, ctx):
         await ctx.send(f'Rolled a dice, it was a {randint(0,6)}!')
 
-    @commands.command(aliases=['d','choose','c'],
-        brief='Chooses between a list of items',
-        description='Returns a random value from a list of items given following the command (seperated by spaces)\nRequires a role of "OG Squad" or higher'
-        )
+    @commands.command(aliases=['d', 'choose', 'c'],
+                      brief='Chooses between a list of items',
+                      description='Returns a random value from a list of items given following the command (seperated by spaces)\nRequires a role of "OG Squad" or higher'
+                      )
     @commands.has_any_role(*allowed_roles)
     async def decide(self, ctx, *, options):
         options = options.split(' ')
@@ -47,7 +48,7 @@ class general_commands(commands.Cog):
 
     @decide.error
     async def decide_error(self, ctx, error):
-        if isinstance(error,commands.MissingRequiredArgument):
+        if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Please specify a list of items to choose from (seperated by spaces)')
         else:
             await ctx.send(f'An error occured, {error}')
@@ -55,7 +56,7 @@ class general_commands(commands.Cog):
     @commands.command(
         brief='Clears a number of messages',
         description='Clears a number of previous messages from a channel\nRequires a role of "Mody Boi" or higher'
-        )
+    )
     @commands.has_any_role(*elevated_roles)
     async def clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=(amount+1))
@@ -63,6 +64,7 @@ class general_commands(commands.Cog):
 
 def setup(client):
     client.add_cog(general_commands(client))
+
 
 def teardown(client):
     pass
