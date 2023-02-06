ac63ad86888b0604be881cf4cd492c53aa0f943c
caleb-wishart <caleb.wish@outlook.com>
2023-02-06 20:20:36 +1000

    Type hint owner

--- bot/cogs/owner.py
+++ bot/cogs/owner.py
@@ -1,9 +1,9 @@
 # owner.py
 
+from datetime import datetime
+
 import discord
 from discord.ext import commands
-
-from datetime import datetime
 
 
 class owner(commands.Cog):
@@ -15,7 +15,7 @@ class owner(commands.Cog):
 
     @commands.command(brief="Loads a cog extention", description="Loads a cog extention", require_var_positional=True)
     @commands.is_owner()
-    async def load(self, ctx, cog):
+    async def load(self, ctx: commands.Context, cog):
         await self.bot.load_extension(f"cogs.{cog}")
         await ctx.reply(f"Loaded {str(cog)}!")
 
@@ -23,7 +23,7 @@ class owner(commands.Cog):
         brief="Unloads a cog extention", description="Unloads a cog extention", require_var_positional=True
     )
     @commands.is_owner()
-    async def unload(self, ctx, cog):
+    async def unload(self, ctx: commands.Context, cog):
         await self.bot.unload_extension(f"cogs.{cog}")
         await ctx.reply(f"Unloaded {str(cog)}!")
         self.log.info(msg=f"{ctx.author} unloaded {str(cog)}")
@@ -32,23 +32,25 @@ class owner(commands.Cog):
         brief="Reloads a cog extention", description="Reloads a cog extention", require_var_positional=True
     )
     @commands.is_owner()
-    async def reload(self, ctx, cog):
+    async def reload(self, ctx: commands.Context, cog):
         await self.bot.reload_extension(f"cogs.{cog}")
         await ctx.reply(f"Reloaded {str(cog)}!")
         self.log.info(msg=f"{ctx.author} reloaded {str(cog)}")
 
     @commands.command(brief="Syncs the application tree", description="Syncs the application tree")
     @commands.is_owner()
-    async def sync(self, ctx, guild_id):
-        if guild_id:
+    async def sync(self, ctx: commands.Context, guild_id=None):
+        if guild_id is not None:
             if guild_id == "guild" or guild_id == "~":
+                if ctx.guild is None:
+                    return
                 guild_id = ctx.guild.id
             tree = await self.bot.tree.sync(guild=discord.Object(id=guild_id))
         else:
             tree = await self.bot.tree.sync()
 
         await ctx.reply(f"{len(tree)} synced!")
-        self.log.info(msg=f"{ctx.author} synced the tree({len(tree)}): {tree}")
+        self.log.info(msg=f"{ctx.author} synced the tree({len(tree)}): {[command.name for command in tree]}")
 
     @commands.command(brief="Shows the bot log", description="Shows the bot log")
     @commands.is_owner()
